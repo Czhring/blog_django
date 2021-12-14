9140f4d308051559b9d26e977b333e7eba26df9c
Ring <ring@ringdembp.attlocal.net>
2021-12-14 15:10:58 -0800

    Add user serializer for sign up route

--- api/models/user.py
+++ api/models/user.py
@@ -17,6 +17,7 @@ class UserManager(BaseUserManager):
         user.is_superuser = True
         user.is_staff = True
         user.save()
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
